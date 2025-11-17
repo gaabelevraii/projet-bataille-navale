adb3f98ff146fc636a704b405f303a7c90da3c76
Gabriel Bady <gabrielbady03@gmail.com>
2025-11-17 13:24:07 +0100

    Implémentation du constructeur de la classe Bateau
    Sont joints les tests des attributs et de leurs comportements par défaut.

--- bateau.py
+++ bateau.py
@@ -1,2 +1,6 @@
 class Bateau:
-    pass
+    def __init__(self, ligne, colonne, longueur=1, vertical=False):
+        self.ligne = ligne
+        self.colonne = colonne
+        self.longueur = longueur
+        self.vertical = vertical
--- test_bateau.py
+++ test_bateau.py
@@ -2,5 +2,24 @@ from bateau import Bateau
 
 
 def test_init():
-    bateau = Bateau()
+    bateau = Bateau(2, 3, 4, True)
     assert isinstance(bateau, Bateau)
+
+
+def test_ligne():
+    bateau = Bateau(2, 3, 4, True)
+    assert bateau.ligne == 2
+
+
+def test_colonne():
+    bateau = Bateau(2, 3, 4, True)
+    assert bateau.colonne == 3
+
+
+def test_longueur():
+    bateau1 = Bateau(2, 3, 4, True)
+    assert bateau1.longueur == 4
+    bateau2 = Bateau(2, 3)
+    assert bateau2.longueur == 1
+    bateau3 = Bateau(2, 3, vertical=True)
+    assert bateau3.longueur == 1
